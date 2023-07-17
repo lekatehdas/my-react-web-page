00fa8824e2a4b37300a4f48a10898e75884fcece
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-07-17 13:34:43 +0200

    Added a logo to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import {HStack, Box, Spacer, Image} from "@chakra-ui/react";
 import NavMenu from "./NavMenu.tsx";
-import logo from '../assets/react.svg';
+import logo from '../assets/logo.svg';
 
 const Navbar = () => {
     return (
@@ -12,11 +12,11 @@ const Navbar = () => {
             <Box flex="1">
                 <h1>Aki S.</h1>
             </Box>
-            <Spacer />
+            <Spacer/>
             <Box flex="1" display="flex" justifyContent="center" width="100%">
-                <Image src={logo} alt="Logo"/>
+                <Image src={logo} alt="Logo" maxHeight="5vh"/>
             </Box>
-            <Spacer />
+            <Spacer/>
             <Box flex="1" display="flex" justifyContent="end" width="100%">
                 <NavMenu/>
             </Box>
