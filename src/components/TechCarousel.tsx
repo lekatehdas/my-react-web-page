e85de732ec57959858ebda4efe191cc13d777f61
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-07-18 09:34:06 +0200

    Build TechCarousel, and added styling

---
+++ src/components/TechCarousel.tsx
@@ -0,0 +1,45 @@
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import {Box, Text} from "@chakra-ui/react";
+
+interface Props {
+  techs: string[];
+}
+
+const TechCarousel = ({ techs }: Props) => {
+  const items = techs.map((tech, index) => (
+    <Box
+        key={index}
+        bg={'secondary'}
+        marginX={1}
+        borderRadius={"xl"}
+    >
+      <Text
+          paddingX={1}
+          textAlign={'center'}
+          color={'main'}
+      >{tech}</Text>
+    </Box>
+  ));
+
+  return (
+    <AliceCarousel
+      autoPlay
+      infinite
+      disableButtonsControls
+      disableDotsControls
+      items={items}
+      autoPlayInterval={3000}
+      responsive={{
+        0: {
+          items: 3,
+        },
+        1024: {
+          items: 3,
+        }
+      }}
+    />
+  );
+};
+
+export default TechCarousel;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,46 +1,66 @@
-import { Box, Button, Text, Image, Stack } from '@chakra-ui/react';
+import {Box, Button, Text, Image, Stack} from '@chakra-ui/react';
 import pic_me from '../assets/me.jpg';
+import TechCarousel from "../components/TechCarousel.tsx";
 
 const About = () => {
-  return (
-    <Box pt={10} pb={10} display='flex' flexDirection='column' alignItems='center' h='100vh' as='section'>
-      <Image
-        src={pic_me}
-        alt="About Image"
-        objectFit='cover'
-        flex='2'
-        borderRadius='xl'
-        shadow='md'
-      />
-      <Box
-        flex='2'
-        pt={5}
-      >
-        <Text
-          fontSize={['2xl', '3xl', '4xl']}
-          textAlign='left'
-          mb={2}
-        >
-          My name is <Text as='span' fontWeight='bold' color='secondary'>Aki</Text>
-        </Text>
-        <Text
-          fontSize={['sm', 'md', 'lg']}
-          textAlign='left'
-          mb={5}
-        >
-          I'm a computer science graduate with a passion for coding.
-        </Text>
-        <Stack direction={['column', 'row']} spacing={4}>
-          <Button colorScheme='highlight' variant='solid' width={['100%', 'auto']}>
-            Download CV
-          </Button>
-          <Button colorScheme='highlight' variant='solid' width={['100%', 'auto']}>
-            Hire Me
-          </Button>
-        </Stack>
-      </Box>
-    </Box>
-  );
+    const techs = [
+        "React", "Angular", "JavaScript", "Typescript",
+        "HTML", "CSS", "Node.js", "Express.js", "Python",
+        "Django", "Java", "Kotlin", "MySQL", "MongoDB",
+        "Firebase", "Hadoop", "Spark", "ML", "Git"
+    ];
+
+    return (
+        <Box pt={10} display='flex' flexDirection='column' alignItems='center' h='100vh' as='section'>
+
+
+            <Image
+                src={pic_me}
+                alt="About Image"
+                objectFit='cover'
+                flex='2'
+                borderRadius='xl'
+                shadow='md'
+                mb={3}
+            />
+
+
+            <TechCarousel techs={techs}/>
+
+
+            <Box
+                flex='2'
+                mt={3}
+                pt={5}
+            >
+
+                <Text
+                    fontSize={['2xl', '3xl', '4xl']}
+                    textAlign='left'
+                    paddingY={3}
+                >
+                    My name is <Text as='span' fontWeight='bold' color='secondary'>Aki</Text>
+                </Text>
+
+                <Text
+                    fontSize={['sm', 'md', 'lg']}
+                    textAlign='left'
+                    paddingY={3}
+                >
+                    I'm a computer science graduate with a passion for coding.
+                </Text>
+
+                <Stack direction={['column', 'row']} spacing={4} paddingY={5}>
+                    <Button colorScheme='highlight' variant='solid' width={['100%', 'auto']} borderRadius={"3xl"}>
+                        Download CV
+                    </Button>
+                    <Button colorScheme='highlight' variant='solid' width={['100%', 'auto']} borderRadius={"3xl"}>
+                        Hire Me
+                    </Button>
+                </Stack>
+            </Box>
+        </Box>
+    );
 };
 
 export default About;
