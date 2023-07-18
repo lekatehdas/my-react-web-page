fda7caaf22548c3c23967661ba2de8f20a78a424
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-07-18 11:10:11 +0200

    Refactored the about page to use grid

--- src/components/TechCarousel.tsx
+++ src/components/TechCarousel.tsx
@@ -3,43 +3,43 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import {Box, Text} from "@chakra-ui/react";
 
 interface Props {
-  techs: string[];
+    techs: string[];
 }
 
-const TechCarousel = ({ techs }: Props) => {
-  const items = techs.map((tech, index) => (
-    <Box
-        key={index}
-        bg={'secondary'}
-        marginX={1}
-        borderRadius={"xl"}
-    >
-      <Text
-          paddingX={1}
-          textAlign={'center'}
-          color={'main'}
-      >{tech}</Text>
-    </Box>
-  ));
+const TechCarousel = ({techs}: Props) => {
+    const items = techs.map((tech, index) => (
+        <Box
+            key={index}
+            bg={'secondary'}
+            marginX={1}
+            borderRadius={"xl"}
+        >
+            <Text
+                paddingX={1}
+                textAlign={'center'}
+                color={'main'}
+            >{tech}</Text>
+        </Box>
+    ));
 
-  return (
-    <AliceCarousel
-      autoPlay
-      infinite
-      disableButtonsControls
-      disableDotsControls
-      items={items}
-      autoPlayInterval={3000}
-      responsive={{
-        0: {
-          items: 3,
-        },
-        1024: {
-          items: 3,
-        }
-      }}
-    />
-  );
+    return (
+        <AliceCarousel
+            autoPlay
+            infinite
+            disableButtonsControls
+            disableDotsControls
+            items={items}
+            autoPlayInterval={3000}
+            responsive={{
+                0: {
+                    items: 3,
+                },
+                1024: {
+                    items: 3,
+                }
+            }}
+        />
+    );
 };
 
 export default TechCarousel;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, Text, Image, Stack} from '@chakra-ui/react';
+import {Box, Button, Text, Image, Stack, Grid, GridItem} from '@chakra-ui/react';
 import pic_me from '../assets/me.jpg';
 import TechCarousel from "../components/TechCarousel.tsx";
 
@@ -11,48 +11,60 @@ const About = () => {
     ];
 
     return (
-        <Box pt={10} display='flex' flexDirection='column' alignItems='center' h='100vh' as='section'>
+        <Grid
+            templateAreas={{
+                base: `"picture"
+                "techCarousel"
+                "textArea"
+                "buttonBox"`
+            }}
+            gridTemplateRows={"1fr min-content min-content min-content"}
+            gridTemplateColumns={'100%'}
+            h='calc(100vh - 45px)'
+            gap={3}
+            paddingY={3}
+        >
+
+            <GridItem gridArea='picture'>
+                <Image
+                    src={pic_me}
+                    alt="About Image"
+                    objectFit='cover'
+                    borderRadius='xl'
+                    shadow='md'
+                    h="100%"
+                    boxSize="full"
+                />
+            </GridItem>
 
 
-            <Image
-                src={pic_me}
-                alt="About Image"
-                objectFit='cover'
-                flex='2'
-                borderRadius='xl'
-                shadow='md'
-                mb={3}
-            />
+            <GridItem gridArea='techCarousel'>
+                <TechCarousel techs={techs}/>
+            </GridItem>
 
+            <GridItem gridArea='textArea'>
+                <Box>
+                    <Text
+                        fontSize={['2xl', '3xl', '4xl']}
+                        textAlign='left'
+                        noOfLines={2}
+                    >
+                        My name is <Text as='span' fontWeight='bold' color='secondary'>Aki</Text>
+                    </Text>
 
-            <TechCarousel techs={techs}/>
+                    <Text
+                        fontSize={['sm', 'md', 'lg']}
+                        textAlign='left'
+                        paddingY={3}
+                        noOfLines={3}
+                    >
+                        I'm a computer science graduate with a passion for coding and problem solving.
+                    </Text>
+                </Box>
+            </GridItem>
 
-
-            <Box
-                flex='2'
-                mt={3}
-                pt={5}
-            >
-
-                <Text
-                    fontSize={['2xl', '3xl', '4xl']}
-                    textAlign='left'
-                    paddingTop={3}
-                    noOfLines={2}
-                >
-                    My name is <Text as='span' fontWeight='bold' color='secondary'>Aki</Text>
-                </Text>
-
-                <Text
-                    fontSize={['sm', 'md', 'lg']}
-                    textAlign='left'
-                    paddingY={3}
-                    noOfLines={[2, 3]}
-                >
-                    I'm a computer science graduate with a passion for coding and problem solving.
-                </Text>
-
-                <Stack direction={['column', 'row']} spacing={2} paddingTop={10}>
+            <GridItem gridArea='buttonBox'>
+                <Stack direction={['column', 'row']} spacing={2} paddingTop={3}>
                     <Button colorScheme='highlight' variant='solid' width={['100%', 'auto']} borderRadius={"3xl"}>
                         Download CV
                     </Button>
@@ -60,8 +72,9 @@ const About = () => {
                         Hire Me
                     </Button>
                 </Stack>
-            </Box>
-        </Box>
+            </GridItem>
+
+        </Grid>
     );
 };
 
