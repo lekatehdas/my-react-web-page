1f491178a4c1a6966844e1477a0fc314cfd7c541
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-07-19 21:15:14 +0200

    Build project page and project Card

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,80 @@
+import {Box, Text, Image, Button, Flex, VStack, HStack} from '@chakra-ui/react';
+import {Techs} from "../Techs.ts";
+import TechStackIconList from "./TechStackIconList.tsx";
+
+export interface Project {
+    title: string;
+    explanation: string;
+    image: string;
+    github: string;
+    page?: string;
+    techs: Techs[]
+}
+
+interface Props {
+    project: Project;
+}
+
+const ProjectCard = ({project}: Props) => {
+    return (
+        <Box
+            padding={4}
+            borderWidth={1}
+            borderRadius="lg"
+            boxShadow="lg"
+            height="100%"
+        >
+            <Flex direction={["column", "row"]} height="100%">
+                <VStack flex="1" pb={3} align={'flex-end'} height="100%">
+                    <Box
+                        height="100%"
+                        width="100%"
+                        overflow={'hidden'}
+                        position="relative"
+                        borderRadius={'md'}
+                    >
+                        <Image
+                            src={project.image}
+                            alt={project.title}
+                            objectFit="cover"
+                            boxSize="100%"
+                            position="absolute"
+                            top="0"
+                            right="0"
+                            bottom="0"
+                            left="0"
+                        />
+                    </Box>
+
+                    <TechStackIconList techs={project.techs}/>
+                </VStack>
+
+
+                <VStack
+                    flex="1"
+                    justify="space-between"
+                    p={2}
+                    gap={1}
+                >
+                    <Box>
+                        <Text fontSize="2xl">{project.title}</Text>
+                        <Text>{project.explanation}</Text>
+                    </Box>
+
+                    <HStack spacing={3}>
+                        <Button as="a" href={project.github} target="_blank" colorScheme="highlight">
+                            GitHub
+                        </Button>
+                        {project.page &&
+                            <Button as="a" href={project.page} target="_blank" colorScheme="highlight">
+                                Live Demo
+                            </Button>
+                        }
+                    </HStack>
+                </VStack>
+            </Flex>
+        </Box>
+    );
+}
+
+export default ProjectCard;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,9 +1,37 @@
+import {projects} from "../projects.ts";
+import ProjectsCarousel from "../components/ProjectsCarousel.tsx";
+import 'react-alice-carousel/lib/alice-carousel.css';
+import {useState} from "react";
+import {Grid, GridItem} from "@chakra-ui/react";
+import ProjectCard from "../components/ProjectCard.tsx";
+
 const Projects = () => {
-    return (
-        <div>
-            Projects
-        </div>
-    );
-};
+    const [selected, setSelected] = useState(projects[0]);
+
+    return <Grid
+        templateAreas={{
+            base: `"text"
+            "carousel"
+            "projectCards"`
+        }}
+        paddingY={10}
+        gap={10}
+        gridTemplateRows={"min-content min-content 1fr"}
+        gridTemplateColumns={'100%'}
+        h='calc(100vh - 45px)'
+    >
+        <GridItem gridArea={'text'}>
+            <h1>Here are some of my projects</h1>
+        </GridItem>
+
+        <GridItem gridArea={'carousel'}>
+            <ProjectsCarousel projects={projects} onSelect={setSelected} />
+        </GridItem>
+
+        <GridItem gridArea={'projectCards'}>
+            <ProjectCard project={selected}/>
+        </GridItem>
+    </Grid>
+}
 
 export default Projects;
