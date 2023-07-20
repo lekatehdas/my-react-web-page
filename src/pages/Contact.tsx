dfa25cd86b0cdc00ebc6ad3e2f68d5760730b416
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-07-20 13:26:14 +0200

    Build contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,91 @@
+import {
+    Box,
+    Button,
+    FormControl,
+    FormLabel,
+    GridItem,
+    Input,
+    Text,
+    Textarea,
+    VStack,
+    Grid
+} from "@chakra-ui/react";
+import React from "react";
+import useForm, {FormProps} from "../hooks/useForm.ts";
+import Toast from "../components/Toast.tsx";
+
 const Contact = () => {
+    const {handleSubmit, success, error} = useForm()
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+
+        const data: FormProps = {
+            name: formData.get('name') as string,
+            email: formData.get('email') as string,
+            message: formData.get('message') as string
+        }
+        handleSubmit(data)
+
+    };
+
     return (
-        <div>
-            This is Contact
-        </div>
+        <Grid
+            gap={6}
+            p={10}
+        >
+            <GridItem>
+                <Text fontSize={'xl'}>Lets work together!</Text>
+                <Text fontSize={'sm'}>Send me a message.</Text>
+            </GridItem>
+
+            <GridItem>
+                <Box
+                    as="form"
+                    padding={4}
+                    borderWidth={1}
+                    borderRadius="lg"
+                    boxShadow="lg"
+                    onSubmit={onSubmit}
+                >
+                    <VStack spacing={4}>
+                        <FormControl isRequired>
+                            <FormLabel htmlFor="name">Name</FormLabel>
+                            <Input type="text" id="name" name="name"/>
+                        </FormControl>
+
+                        <FormControl isRequired>
+                            <FormLabel htmlFor="email">Email address</FormLabel>
+                            <Input type="email" id="email" name="email"/>
+                        </FormControl>
+
+                        <FormControl isRequired>
+                            <FormLabel htmlFor="message">Message</FormLabel>
+                            <Textarea id="message" name="message" maxLength={1024}/>
+                        </FormControl>
+
+                        <Button type="submit" colorScheme="highlight">
+                            Submit
+                        </Button>
+                    </VStack>
+                </Box>
+            </GridItem>
+            {success && (
+                <Toast
+                    title="Message sent."
+                    description="I will get back to you soon."
+                    status="success"
+                />
+            )}
+            {error && (
+                <Toast
+                    title="An error occurred."
+                    description="Unable to send your message. Please try again."
+                    status="error"
+                />
+            )}
+        </Grid>
     );
 };
 
