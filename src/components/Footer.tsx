2079f44d9e4c71d3f8214cd70c7b55a3c19f1d98
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-07-20 22:02:27 +0200

    Build footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,21 @@
+import {Box, HStack, Link} from '@chakra-ui/react';
+import {SiGithub, SiLinkedin} from 'react-icons/si';
+import {Icon} from "@chakra-ui/icons";
+
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <HStack>
+            <Box>&copy; 2023 Aki Sirkiä</Box>
+
+            <Link href="https://github.com/lekatehdas" isExternal>
+                <Icon as={SiGithub} color={'secondary'}/>
+            </Link>
+
+            <Link href="https://www.linkedin.com/in/aki-sirki%C3%A4-5397a8272/" isExternal ml={2}>
+                <Icon as={SiLinkedin} color={'secondary'}/>
+            </Link>
+        </HStack>
+
     );
 };
 
